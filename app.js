d03a7279b8c2e9a75957ac9a63c58e093fd9e26c
kopi22 <kpijanowski99@gmail.com>
2018-12-01 13:02:48 +0000

    First lines of code

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-window.onload = function() {
-  function onFrame (event) {
+var troopers = [];
+var bullets = [];
+var debris = [];
+var helicopters = [];
+var turret;
 
-  }
+// Debris Helicopter Trooper
+
+window.onload = function() {
+  turret = new Raster('turret');
+  turret.position = new Point(200,380);
+
+  path = new Path.Rectangle({
+  	point: [75, 75],
+  	size: [75, 75],
+  	strokeColor: 'black'
+  });
 }();
+
+function onFrame(event) {
+  // Each frame, rotate the path by 3 degrees:
+  if(Key.isDown('right')){
+    turret.rotate(90);
+  } else if(Key.isDown('left')) {
+    turret.rotate(-3);
+  }
+}
