5ecba87a7333220c61c7b4a81dee38ae4e78f91f
Carlin Mackenzie <carlin.mackenzie@gmail.com>
2018-12-01 13:50:38 +0000

    added heli constructor

--- js/Helicopter.js
+++ js/Helicopter.js
@@ -1,12 +1,20 @@
 class Helicoptor {
   constructor() {
-    this.x = 0;
-    this.y = 0;
+    if (Math.round(Math.random())) {
+      this.direction = 'l';
+      this.x = 400;
+      this.y = 20;
+    } else {
+      this.direction = 'r';
+      this.x = 0;
+      this.y = 20;
+    }
     this.velocity = 0;
   }
 
-  // Change [0, 2] to A ♦
-  display() {}
+  display() {
+    return (this.x, this.y, this.direction);
+  }
 
   hit() {}
 }
