92e59338d3dc7d55554dfc6aced6b20d5758d860
Gabriel Ohowa Owino <s1871518@Asperger17-2.local>
2018-12-01 13:53:51 +0000

    bullet and turret updates

--- js/Turret.js
+++ js/Turret.js
@@ -1,11 +1,18 @@
-class Turret {
+export default class Turret {
   constructor() {
     this.angle = 0;
   }
 
-  // Change [0, 2] to A ♦
-  fire() {}
-  rotate() {}
-  hit() {}
-  display() {}
+  get angle(){
+    return this._angle;
+  }
+
+  set angle(angle){
+    if (angle>=0 && angle<= Math.pi){
+      this._angle = angle;
+    } else {
+      throw 'Angle beyond the exceeded amount';
+    }
+  }
+
 }
